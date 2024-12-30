f8f2b3b952619df1ee0f3be26c7dd03480a9a0a8
Duy2902 <tduy29.2k4@gmail.com>
2024-12-30 21:38:48 +0700

    constant

--- app/src/main/java/com/example/devicemonitoring/Constants.java
+++ app/src/main/java/com/example/devicemonitoring/Constants.java
@@ -7,4 +7,6 @@ public class Constants {
     // Các hằng số khác (nếu cần)
     public static final int TIMEOUT_DURATION = 5000; // Ví dụ: thời gian chờ kết nối
     public static final int TIMEOUT_SCREENSHOT = 10000;
+
+
 }
